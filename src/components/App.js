b4f4d1417c23f27e3bce1a06779a2148b9380d95
Yura Sabat <yrasabat@gmail.com>
2022-07-25 18:54:55 +0300

    start

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import SearchArea from "./SearchArea";
+
+function App() {
+  return (
+    <div>
+        <Nav/>
+        <SearchArea/>
+    </div>
+  );
+}
+
+export default App;
